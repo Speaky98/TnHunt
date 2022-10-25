c0bf828a0513404f240db0191c889e83a2fefab8
Anis <anis.kraiem1996@gmail.com>
2022-10-25 23:08:28 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,13 @@ use App\Http\Controllers\EventsController;
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\ProgChasseController;
 use App\Http\Controllers\ParticiperprogController;
-
+use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\AnimalControllerFront;
+use App\Http\Controllers\ChienController;
+use App\Http\Controllers\ChienControllerFront;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RayonController;
+use App\Models\Rayon;
 
 
 /*
@@ -44,4 +50,54 @@ Route::resource('/progchasse', ProgChasseController::class);
 Route::resource('/participerprog', ParticiperprogController::class);
 Route::get('/add/{id}',[ParticiperprogController::class,'add']);
 Route::post('/save/{id}',[ParticiperprogController::class,'createParticipant']);
+Route::get("/Front",function (){
+    return view("layouts.Client.main");
+})->name("frontpage");
 
+Route::get('/Dashboard', function () {
+    $route = Route::current()->getName();
+    return view('layouts.Dashboard.main')->with('route',$route);
+})->name("backpage");;
+
+
+Route::get("/test",[ProductController::class,'indextest'])->name("testtest");
+
+/********Dashboard product*********/
+
+Route::get("/Product",[ProductController::class,'index'])->name("productpage");
+
+Route::post("/Ajoutafterproduct",[ProductController::class,'Add'])->name("Ajoutprod2");
+
+Route::get("/Modifbeforeproduct/{id}",[ProductController::class,'Modifybefore'])->name("Modifyprod1");
+
+Route::post("/Modifafterproduct",[ProductController::class,'Modify'])->name("Modifyprod2");
+
+Route::get("/Deleteproduct/{id}",[ProductController::class,'Delete'])->name("Deleteprod1");
+
+/********Dashboard rayon*********/
+
+Route::get("/Rayon",[RayonController::class,'index'])->name("rayonpage");
+
+Route::post("/Ajoutafterrayon",[RayonController::class,'Add'])->name("Ajoutray2");
+
+Route::get("/Modifbeforerayon{id}",[RayonController::class,'Modifybefore'])->name("Modifyray1");
+
+Route::post("/Modifafterrayon",[RayonController::class,'Modify'])->name("Modifyray2");
+
+Route::get("/Deleterayon/{id}",[RayonController::class,'Delete'])->name("Deleteray1");
+
+/********Client*********/
+
+Route::get("/Productclient",[ProductController::class,'indexclient'])->name("productpageclient");
+
+Route::post("/Ajoutafterrate",[ProductController::class,'Addrate'])->name("Modifyrate2");
+
+/***********Tarak*******/
+
+Route::resource('animals', AnimalController::class);
+
+Route::resource('animalsFront', AnimalControllerFront::class);
+
+Route::resource('chiens', ChienController::class);
+
+Route::resource('chiensFront', ChienControllerFront::class);
