98c8726e78a0cd558dffd2fc0b9fb4f9aeb3529d
speaky98 <selimbenaichesprit@gmail.com>
2022-10-26 01:33:12 +0100

    (++++ Integration)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
 
     public function __construct()
     {
-        $this->middleware('guest', ['except' => ['Rayon']]);
+        $this->middleware('auth', ['except' => ['Product']]);
     }
 
     public function indextest()
